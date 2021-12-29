f432bf51992ea82146224172c88c8bd49c171726
niemczus <87914966+niemczus@users.noreply.github.com>
2021-12-29 09:33:40 +0100

    Initial Commit

---
+++ Practice Project/Practice_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_ProjectApp.swift
+//  Practice Project
+//
+//  Created by Kamil on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Practice_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
